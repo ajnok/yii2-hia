e85d2e1cdd5c6ad88e88218b2e20292f8649ff24
ajnok <ajnokit@gmail.com>
2015-02-02 23:49:15 +0700

    add php server script for mac

---
+++ tmp/web_backend.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /Applications/MAMP/htdocs/yii2-hia
+php -S localhost:8081 -t backend/web
---
+++ tmp/web_fronend.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /Applications/MAMP/htdocs/yii2-hia
+php -S localhost:8080 -t frontend/web
