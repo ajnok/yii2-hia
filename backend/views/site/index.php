dc147b630ccce9a9dfcf0ed504685e351bcdc1de
ajnok <ajnokit@gmail.com>
2015-02-04 12:38:48 +0700

    Revert "add 4 model in controller"
    This reverts commit be38f2c275396b3075831962143ff983511537e2.

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -4,9 +4,7 @@ $this->beginBlock('leftbar', false);
 <h3>1</h3>
 <?php
 $this->endBlock();
-?> 
-
-
+?>
 <div class="row">
     <div class="col-lg-4">
         <h2>Heading</h2>
@@ -16,15 +14,33 @@ $this->endBlock();
             ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
             fugiat nulla pariatur.</p>
 
-        <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-    </div>
-    <div class="col-lg-4"">
-        <h2>Heading</h2>
+    
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-            fugiat nulla pariatur.</p>
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4" style="border: 1px solid">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            </div>
+            <div class="col-lg-4" style="border: 1px solid">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            </div>
+            <div class="col-lg-4" style="border: 1px solid">
+                <h2>Heading</h2>
 
         <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
     </div>
